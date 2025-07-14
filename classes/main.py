b8576550a38b7ed9c38198ae8b6bde4b7fc1ab82
Shaniagrg <sglearner2022@gmail.com>
2025-07-13 18:59:11 -0700

    use nested function in classes

--- classes/main.py
+++ classes/main.py
@@ -1,10 +1,23 @@
 class Bank:
     checking:float = 0.0
     saving:float = 0.0 
-    def __inhit__ (self,c,s):
+    def __init__ (self,c:float,s:float):
         self .checking = c
         self.saving = s
 
 #Bank CA = new CA (10.00,20.00)
 
 CA:Bank = Bank(10.00,20.00)
+
+
+class Person:
+  def __init__(self, n, a):
+    self.name = n
+    self.age = a
+
+  def __str__(self):
+    return f"{self.name}({self.age})"    
+
+p1:Person = Person("John", 36)
+
+print(p1)

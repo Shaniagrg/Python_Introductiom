2e125d2defc13ef48d1951cced58cb16a589fddf
Shaniagrg <sglearner2022@gmail.com>
2025-07-27 22:08:06 -0700

    Use class function inside class

--- classes/README.md
+++ classes/README.md
@@ -2,8 +2,8 @@
 <ol>
     <li>CAPS: Written in CAPS</li>
     <li>__intit__ : Use  __intit__ to call constructor</li>
-    <li>self: Use self as paramenter for contructor call</li>
-    <li>@classmethod: Use @classmethod for class function</li>
+    <li>self: Use self as paramenter for instance function  def function_name(self):</li>
+    <li>@classmethod: Use @classmethod for class function and (cls) as parameter  def function_name(cls):</li>
     <li></li>
     <li></li>
     <li></li>
--- classes/main.py
+++ classes/main.py
@@ -1,3 +1,4 @@
+
 class Bank:
     checking:float = 0.0
     saving:float = 0.0 
@@ -11,8 +12,14 @@ CA:Bank = Bank(10.00,20.00)
 
 
 class Person:
-  def __init__(self, n, a):
-    self.name = n
+  '''
+  Person()
+    this.name = n
+    this.name = a
+  but instead in python we write __init__ and self for constructor
+  '''
+  def __init__(self, n, a):   #we usually write Person() but in python the CONSTRUCTOR is calle by __init__
+    self.name = n             # self is always in the parameter inside class which also refer to this.name = n
     self.age = a
 
   def __str__(self):
@@ -21,3 +28,30 @@ class Person:
 p1:Person = Person("John", 36)
 
 print(p1)
+
+#Homework 1
+class Example:
+  cv:str = "I'm class variable"
+  iv:str = ""
+  def __init__(self):
+     self.iv = "I'm instance variable"
+  @classmethod     #to call class function we usually use static function cm{...} but in python you call a DECLATOR @classmethod
+  def cm(cls):     #use cls as parameter always
+     print("This is class function")
+  def im(self):
+     print("This is instance function.")
+
+'''
+Example obj = new Example()
+we studies this way but will skip new in python
+'''
+obj:Example = Example()
+
+print(obj.cv)
+print(obj.iv)
+
+obj.cm()
+obj.im()
+
+#Example.im() Throw error cuz class don't have access to instance FUNCTION
+#print(Example.iv) Throw error cuz class don't have access to instance VARIABLE

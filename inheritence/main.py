703055f4ec7198a9534e1189fe9ed764739569b4
Shaniagrg <sglearner2022@gmail.com>
2025-10-25 23:03:27 -0700

    use inheritance in class

---
+++ inheritence/main.py
@@ -0,0 +1,24 @@
+class Animal:
+    legs:int = 0 
+    def __init__(self,l:int):
+        self.legs = l
+    
+    def speak(self) -> str:
+        return "howl"
+    def getLegs(self) -> int:
+        return self.legs
+    def test(self)->str:
+        return "test"
+    
+class Dog(Animal):
+    def __init__(self,l:int):
+        super().__init__(l)
+    def speak(self)->str:
+        return "woof"
+    
+d1:Dog = Dog(4)
+print(d1.getLegs())
+print(d1.speak())
+print(d1.test())
+    
+        

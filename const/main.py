8ac8a35006a5f297f5fa6bc88260429ad41a1361
Shaniagrg <sglearner2022@gmail.com>
2025-11-04 20:10:37 -0800

    use const to understand the concept

---
+++ const/main.py
@@ -0,0 +1,18 @@
+'''
+    Const
+    - close ended box with all upper case
+    - value cant be changed or modified
+'''
+a:int = 10
+B:int = 5
+a = 2
+print (a)
+print(B)
+'''
+
+B = 55
+print(B)
+
+    this works in python but noramlly since B is constant so throws error
+
+'''

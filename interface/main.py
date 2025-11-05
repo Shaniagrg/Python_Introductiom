758881445343f50f10dd0b8475653fe3b6ea7740
Shaniagrg <sglearner2022@gmail.com>
2025-11-04 20:53:00 -0800

    use interface inside class

---
+++ interface/main.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    legs:int = 0
+    def __init__(self, l):
+        self.legs = l
+        
+    @abstractmethod
+    def noise(self) -> str:
+        return f'Some noise, {self.legs} legs.'
+
+    @abstractmethod
+    def move(self) -> str:
+        return 'on land'
+    
+    @classmethod
+    def test(cls) -> str:
+        return 'test'
+        
+        
+class Dog (Animal):
+    breed:str = ""
+    
+    def __init__(self, l:int, b:str):
+        super().__init__(l)
+        self.breed = b
+        
+    def noise(self) -> str:
+        return f'{super().noise()} woof'
+    
+    def move(self) -> str:
+        return 'run'
+    
+    def walk(self):
+        return f'Dog walks {super().move()}'
+    
+d1:Dog = Dog (1, 'BullDog')
+print(d1.move())
+print(d1.noise())
+print(d1.test())
+

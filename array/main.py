e582acf169be3058898967094b736efffeed4e4f
Shaniagrg <sglearner2022@gmail.com>
2026-01-16 02:11:46 -0800

    use function to test the highest_value

--- array/main.py
+++ array/main.py
@@ -3,12 +3,19 @@ Find the largest element in the array
 Eg: [1,3,6,2]
 ans: 6
 '''
-a:list[int] = [1,3,6,2]
 
-x:int = 0
+def highest_value (a:list[int]) -> int:
+    x:int = 0
+    for i in range(len(a)):
+        if x < a[i]:
+            x = a[i]
+    return x
+def highest_valueTest(expected:int, actual:int):
+    if expected == actual:
+        return "correct"
 
-for i in range(len(a)):
-    if x < a[i]:
-        x = a[i]
+    else:
+        return "incorrect, addition is wrong"
 
-print(x)
+print(highest_valueTest(6, highest_value([1,3,6,2])))
+print(highest_valueTest(4, highest_value([1,3,4])))

89413b36d67f1f9c76da6fe14fe9411aa13a90fc
Shaniagrg <sglearner2022@gmail.com>
2026-01-16 02:41:24 -0800

    use funtion to test the sum of 2d row array

--- array/2d_row.py
+++ array/2d_row.py
@@ -3,22 +3,23 @@ Get the sum of the row
 eg: [[4,1],[5,2],[6,3]]
 ans: 5,7 & 9
 '''
-a:list[int] = [[4,1],[5,2],[6,3]]
-x:int = 0
-y:int = 0
-z:int = 0
 
-for i in range(len(a[0])):  #len(a[0])
-    x = x + a[0][i]
-          
-print(x)
+def sum_row (a:list[int]):
+    sum:list[int] = []
+    for row in range(len(a)):
+        total:int = 0
+        for index in range(len(a[row])):
+            total = total + a[row][index]
+        sum.append(total)
+    return sum
 
-for ind in range(len(a[1])):
-    y = y + a[1][ind]
+def addTest(expected:list[int], actual:list[int]):
+    if expected == actual:
+        return "correct"
 
-print(y)
-
-for index in range(len(a[2])):
-    z = z + a[2][index]
+    else:
+        return "incorrect, addition is wrong"
+        
+print (addTest([5,7,9] , sum_row([[4,1],[5,2],[6,3]])))
+print (addTest([6,4,3] , sum_row([[3,3],[2,2],[2,1]])))
     
-print(z)

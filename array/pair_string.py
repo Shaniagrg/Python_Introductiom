febf0bfe4eb72c998e6f52a3da6e4242c580d194
Shaniagrg <sglearner2022@gmail.com>
2026-01-16 03:17:42 -0800

    Implement get_value function to form pairs from the first character and odd indexed characters in a string

---
+++ array/pair_string.py
@@ -0,0 +1,24 @@
+'''
+a = "abcd"
+ans = "ad" "ab"
+'''
+
+def get_value (a:str) -> list[str]:
+    alphabet:list[str] = []
+    
+    for i in range(len(a)):
+        if i % 2 == 1: #check for odd number 
+            pair:str = a[0] + a[i]
+            alphabet.append(pair)
+        
+    return alphabet
+
+def addTest(expected:list[str], actual:list[str]):
+    if expected == actual:
+        return "correct"
+
+    else:
+        return "incorrect, addition is wrong"
+    
+print(addTest(['ab','ad'], get_value('abcd')))
+print(addTest(['ef','eh'], get_value('efgh')))

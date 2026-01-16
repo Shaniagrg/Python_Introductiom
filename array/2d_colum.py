30bdb893f78a99b76e22a1d600e3b265a89cc79f
Shaniagrg <sglearner2022@gmail.com>
2026-01-16 02:58:25 -0800

    use funtion to test sum of 2d array column

--- array/2d_colum.py
+++ array/2d_colum.py
@@ -3,18 +3,30 @@ Get sum of the column
 eg: [[4,1],[5,2],[6,3]]
 ans: 15 & 6
 '''
-a:list[int] = [[4,1],[5,2],[6,3]]
-x:int = 0
-y:int = 0
 
-for i in range(len(a)):
-    x = x + a[i][0]
-          
-print(x)
+def sum_column(a:list[int]) -> list[int]:
+    sum:list[int] = []
+    total_column:int = len(a[0])  
+    
+    for column in range(total_column):
+        total:int = 0
+        
+        for index in range(len(a)):
+            total = total + a[index][column]
+            
+        sum.append(total)
+        
+    return sum
 
-for i in range(len(a)):
-    y = y + a[i][1]
+def addTest(expected:list[int], actual:list[int]):
+    if expected == actual:
+        return "correct"
 
-print(y)
+    else:
+        return "incorrect, addition is wrong"
+
+print(addTest([15,6], sum_column([[4,1],[5,2],[6,3]])))
+print(addTest([8,10], sum_column([[4,1],[2,5],[2,4]])))
+            
        
 

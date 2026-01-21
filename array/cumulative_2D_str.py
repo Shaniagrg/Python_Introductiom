d3cbfb619ed994db48719285bd7bc78ccdc8397b
Shaniagrg <sglearner2022@gmail.com>
2026-01-21 01:26:51 -0800

    Arrange string into 2d array

---
+++ array/cumulative_2D_str.py
@@ -0,0 +1,31 @@
+'''
+eg: "cow"
+
+ans: [['c','co', 'cow'], ['o', 'ow'], ['w']]
+'''
+
+
+def get_cumulative_str (a:str) -> list[str]:
+    store_add:list[list[str]] = []
+    for i in range(len(a)):
+        value = ""
+        add:list[str] = []
+        for j in range(i, len(a)): 
+            value = value + a[j]  
+            add.append(value)
+        store_add.append(add)
+              
+    return store_add
+
+def addTest(expected:list[list[str]], actual:list[list[str]]) -> str:
+    print(actual)
+    for i in range(len(expected)):
+        if expected[i] == actual[i]:
+            return "correct"
+
+        else:
+            return "incorrect, addition is wrong"
+
+print(addTest([['c','co', 'cow'], ['o', 'ow'], ['w']], get_cumulative_str("cow") ))
+print(addTest([['p','pe', 'pen'], ['e', 'en'], ['n']], get_cumulative_str("pen") ))
+

0eca3f51a8f6ce635064849a46e9cf9893edc6ae
Shaniagrg <sglearner2022@gmail.com>
2026-01-21 00:48:28 -0800

    update addTest function for edge cases like empty array and string

--- array/string.py
+++ array/string.py
@@ -7,17 +7,17 @@ def get_value (a:str) -> list[str]:
     alphabet:list[str] = []
     
     for i in range(len(a)):
-        alphabet.append(a[i])
+        alphabet.append(a[i])  
         
     return alphabet
 
 def addTest(expected:list[str], actual:list[str]) -> str:
     for i in range(len(expected)):
-        if expected[i] == actual[i]:
-            return "correct"
-
-        else:
+        if expected[i] != actual[i]:
             return "incorrect, addition is wrong"
+    return "correct"
+
     
 print(addTest(['a','b','c','d'], get_value('abcd')))
 print(addTest(['e','f','g','h'], get_value('efgh')))
+print(addTest([], get_value(''))) #edge cases like empty array and string

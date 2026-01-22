2313182a20aeca04ca0bb8afd3e274f020969363
Shaniagrg <sglearner2022@gmail.com>
2026-01-22 00:27:22 -0800

    use conditionals if the array is empty for testing

--- array/cumulative_2D_str.py
+++ array/cumulative_2D_str.py
@@ -7,13 +7,17 @@ ans: [['c','co', 'cow'], ['o', 'ow'], ['w']]
 
 def get_cumulative_str (a:str) -> list[str]:
     store_add:list[list[str]] = []
-    for i in range(len(a)):
-        value = ""
-        add:list[str] = []
-        for j in range(i, len(a)): 
-            value = value + a[j]  
-            add.append(value)
-        store_add.append(add)
+    if len(a) == 0:
+        store_add.append([''])
+        return store_add
+    else:
+        for i in range(len(a)):
+            value = ""
+            add:list[str] = []
+            for j in range(i, len(a)): 
+                value = value + a[j]  
+                add.append(value)
+            store_add.append(add)
               
     return store_add
 
@@ -22,10 +26,13 @@ def addTest(expected:list[list[str]], actual:list[list[str]]) -> str:
     for i in range(len(expected)):
         if expected[i] == actual[i]:
             return "correct"
-
         else:
             return "incorrect, addition is wrong"
+            
 
 print(addTest([['c','co', 'cow'], ['o', 'ow'], ['w']], get_cumulative_str("cow") ))
 print(addTest([['p','pe', 'pen'], ['e', 'en'], ['n']], get_cumulative_str("pen") ))
+print(addTest([['']], get_cumulative_str('') ))
+
+#[['']], ('')
 

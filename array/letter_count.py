d6f014a00b3b6de45bd75de2c674dfe1d504edec
Shaniagrg <sglearner2022@gmail.com>
2026-01-22 01:39:12 -0800

    use map to get the each letter count

---
+++ array/letter_count.py
@@ -0,0 +1,33 @@
+'''
+return each letter count
+
+eg: "hello"
+
+ans = h:1 e:1 l:2 o:1
+'''
+
+def highest_letter (a:str) -> list[str]:
+    letter:str =  ""
+    count:int = 0
+    store_letter:dict[str,int] = {}
+    for i in range(len(a)):
+        letter = a[i]
+        count = 0
+        for each_letter_index in range(len(a)):
+            if letter == a[each_letter_index]:
+                count = count + 1
+        store_letter[letter] = count
+       
+    print(store_letter)
+    return store_letter
+
+
+    
+def highest_letterTest(expected:dict[str,int], actual:dict[str,int]) -> str:
+    if expected == actual:
+        return "correct"
+
+    else:
+        return "incorrect, addition is wrong"
+    
+print(highest_letterTest({'h':1, 'e':1, 'l':2, 'o':1},highest_letter("hello")))

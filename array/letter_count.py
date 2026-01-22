2c7ad078c3dad0c007690ff32f93769ce9db369b
Shaniagrg <sglearner2022@gmail.com>
2026-01-22 01:43:21 -0800

    Use conditionals for edge cases lime empty map

--- array/letter_count.py
+++ array/letter_count.py
@@ -10,14 +10,19 @@ def highest_letter (a:str) -> list[str]:
     letter:str =  ""
     count:int = 0
     store_letter:dict[str,int] = {}
-    for i in range(len(a)):
-        letter = a[i]
-        count = 0
-        for each_letter_index in range(len(a)):
-            if letter == a[each_letter_index]:
-                count = count + 1
-        store_letter[letter] = count
-       
+    if len(a) == 0:
+        store_letter[a] = count
+        print(store_letter)
+        return store_letter
+    else:
+        for i in range(len(a)):
+            letter = a[i]
+            count = 0
+            for each_letter_index in range(len(a)):
+                if letter == a[each_letter_index]:
+                    count = count + 1
+            store_letter[letter] = count
+        
     print(store_letter)
     return store_letter
 
@@ -31,3 +36,4 @@ def highest_letterTest(expected:dict[str,int], actual:dict[str,int]) -> str:
         return "incorrect, addition is wrong"
     
 print(highest_letterTest({'h':1, 'e':1, 'l':2, 'o':1},highest_letter("hello")))
+print(highest_letterTest({'':0},highest_letter('')))

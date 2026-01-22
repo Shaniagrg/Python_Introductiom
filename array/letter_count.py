3b821ed843c9f0e700fa383cbe087fecd9566b6f
Shaniagrg <sglearner2022@gmail.com>
2026-01-22 01:44:15 -0800

    organize the space

--- array/letter_count.py
+++ array/letter_count.py
@@ -10,23 +10,24 @@ def highest_letter (a:str) -> list[str]:
     letter:str =  ""
     count:int = 0
     store_letter:dict[str,int] = {}
+    
     if len(a) == 0:
         store_letter[a] = count
         print(store_letter)
         return store_letter
+    
     else:
         for i in range(len(a)):
             letter = a[i]
             count = 0
+            
             for each_letter_index in range(len(a)):
                 if letter == a[each_letter_index]:
                     count = count + 1
             store_letter[letter] = count
-        
+
     print(store_letter)
     return store_letter
-
-
     
 def highest_letterTest(expected:dict[str,int], actual:dict[str,int]) -> str:
     if expected == actual:
